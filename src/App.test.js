0d5ed6b5c216632c4670e2258c7fd1760a5528f8
Caleb Blackwell <calebxblackwell@gmail.com>
2018-04-09 13:04:06 -0700

    fifth commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import Redux from 'redux'; 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('addToDo', () => {
+  it ('should add new todo', () => {
+    let state={
+      todo: todo
+    };
+    state= trelloReducer(state, addToDo(todoText, 0));
+    expect(state).toEqual({
+      todo: [{todo:[todoText]}]
+    })
+  })
+})
